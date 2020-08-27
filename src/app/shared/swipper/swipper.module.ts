3b4f807ad37ab47f9f6b76b5c5164200c37cef1d
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-08-26 20:49:04 -0500

    Editado todo el diseño de la landing page para ReD

--- src/app/shared/swipper/swipper.module.ts
+++ src/app/shared/swipper/swipper.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 
-import { SwiperModule } from 'ngx-swiper-wrapper';
-import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
-import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { SwiperModule, SwiperConfigInterface, SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+
 import { CommonModule } from '@angular/common';
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   direction: 'horizontal',
-  slidesPerView: 'auto'
+  slidesPerView: 3
+};
+
+const HOME_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  autoplay: true,
+  slidesPerView: 3,
+  breakpoints: {
+    1000: {
+      slidesPerView: 3
+    },
+    478: {
+      slidesPerView: 2
+    },
+    200: {
+      slidesPerView: 1
+    }
+  }
 };
 
 @NgModule({
@@ -22,6 +38,10 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     {
       provide: SWIPER_CONFIG,
       useValue: DEFAULT_SWIPER_CONFIG
+    },
+    {
+      provide: SWIPER_CONFIG,
+      useValue: HOME_SWIPER_CONFIG
     }
   ]
 })
