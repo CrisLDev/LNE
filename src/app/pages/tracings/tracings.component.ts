66af2fb60ed3598f9cff1f33e7223465dcd1e7ea
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-10-19 13:16:12 -0500

    agregado el editar y eliminar de seguimientos

--- src/app/pages/tracings/tracings.component.ts
+++ src/app/pages/tracings/tracings.component.ts
@@ -33,4 +33,27 @@ export class TracingsComponent implements OnInit {
     )
   }
 
+  deleteTracing(tracing_id){
+    document.getElementById("accordionTracings").classList.add("d-none");
+    document.getElementById("spinnerTracings").classList.replace("d-none", "d-block");
+    this.tracingService.deleteTrancingById(tracing_id)
+      .subscribe(
+        res => {
+          document.getElementById("accordionTracings").classList.remove("d-none");
+          document.getElementById("spinnerTracings").classList.replace("d-block","d-none");
+          this.tracings.splice(this.tracings.findIndex(e => e._id === tracing_id), 1);
+          this.toastr.error('Seguimiento eliminado correctamente');
+          if(this.tracings.length <= 0){
+            this.router.navigate(['/home'])
+          }},
+        err => {
+          this.toastr.error(err.error.errors[0].msg);
+        }
+      )
+  }
+
+  editTracing(id, tracing_id){
+    this.router.navigate(['/patients/view/' + id + '/edit/' + tracing_id]);
+  }
+
 }
